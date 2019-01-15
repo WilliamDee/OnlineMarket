b43bed515aba8ec6a607489c9a4054b34c1ed65b
WilliamDee <william.deng14@gmail.com>
2019-01-15 17:57:06 -0500

    changed table name

--- db/db.sql
+++ db/db.sql
@@ -1,12 +1,16 @@
+-- create database
 CREATE DATABASE IF NOT EXISTS store;
 
+-- use database
 use store;
 
-CREATE TABLE products (
+-- create new table
+CREATE TABLE product (
 	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	title VARCHAR(255) NOT NULL,
 	price DECIMAL(65, 2) NOT NULL,
 	inventory_count INT(10) NOT NULL
 );
 
-describe products;
+-- describe the table fields
+describe product;
