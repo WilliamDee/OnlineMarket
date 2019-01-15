7de241623866ef73da171ab5acc477f849d9c65f
WilliamDee <william.deng14@gmail.com>
2019-01-15 17:59:05 -0500

    added total price of all items in cart

--- models/cart.js
+++ models/cart.js
@@ -1,22 +1,32 @@
 module.exports = function Cart(cart){
 	this.items = cart.items || {};
+	this.totalPrice = cart.totalPrice || '0';
 
+	// adding an item to the cart
 	this.add = function(id, product){
-		var cartItem = this.items.id;
-		if(!cartItem){
-			product[quantity] = 1;
+		var cartItem = this.items[id];
+		// Increment cart details. If item doesn't exist in cart, add new entry
+		if(!cartItem){ 
+			product['quantity'] = 1;
+			product['itemPriceTotal'] = product.price;
 			this.items[id] = product;
 		} else {
 			cartItem.quantity++
+			cartItem.itemPriceTotal = cartItem.price * cartItem.quantity;
 		}
+		this.totalPrice = ((+this.totalPrice) + product.price).toFixed(2); // Rounds total price to 2 decimal places
 	}
 
+	// removing an item from the cart
 	this.remove = function(id){
-		var cartItem = this.items.id;
+		var cartItem = this.items[id];
+		// Only remove from the cart if the given product_id exists in the cart
 		if(cartItem){
 			cartItem.quantity--;
+			cartItem.itemPriceTotal -= cartItem.price;
+			this.totalPrice = ((+this.totalPrice) - cartItem.price).toFixed(2) // Rounds total price to 2 decimal places
 			if(cartItem.quantity <= 0){
-				delete this.items.id
+				delete this.items[id];
 			}
 		}
 	}
