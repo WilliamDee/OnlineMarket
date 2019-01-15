5f657f5cd0aac782b9d002a98758cc29f5695a69
WilliamDee <william.deng14@gmail.com>
2019-01-15 18:00:51 -0500

    add product, change case convention, removed possible api hanging

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var router = require('express').Router();
 
-var Cart = require('../models/cart')
+var Cart = require('../models/cart');
 
+// Store homepage, retrieves all products from database
 router.get('/', function(req, res){
 	res.locals.connection.query('SELECT * FROM product', function(err, results, fields){
 		if(err){
@@ -13,34 +14,56 @@ router.get('/', function(req, res){
 	})
 });
 
-router.get('/addToCart/:productId', function(req, res){
-	const productId = req.params.productId;
-	var cart = new Cart(req.session.cart ? req.session.cart : {});
 
+// Adds a product with the given productId to the cart
+router.get('/add_to_cart/:productId', function(req, res){
+	const productId = req.params.productId;
+	var cart = new Cart(req.session.cart ? req.session.cart : {}); // retrieves current cart if it exists, else initiate a new cart
 	res.locals.connection.query(`SELECT * FROM product WHERE id=${productId}`, function(err, results, fields){
 		if(err || results.length == 0){
 			console.error(err ? err : 'Unable to retrieve product');
-			res.status(500).send(err ? err : 'Unable to retrieve product');
+			res.sendStatus(500);
 		} else {
 			var productDetails = results[0];
 			delete productDetails.id;
 			delete productDetails.inventory_count;
-			cart.add(productId, productDetails);
+			cart.add(productId, productDetails); // Adds the selected product to the cart
+			req.session.cart = cart; // Update current cart with the updated cart
+			res.sendStatus(200);
 		}
 	})
-
-	res.redirect('/');
 });
 
-router.get('removeFromCart/:productId', function(req, res){
+// Removes the selected product from the cart 
+router.get('/remove_from_cart/:productId', function(req, res){
 	const productId = req.params.productId;
-	var cart = new Cart(req.session.cart ? req.session.cart : {});
-	cart.remove(productId)
-
-	res.redirect('/');
-})
-router.get('/cart', function(req, res){
-	res.status(200).send('at the cart');
+	var cart = new Cart(req.session.cart ? req.session.cart : {}); // retrieves current cart if it exists, else initiate a new cart
+	cart.remove(productId); // removes the selected product to the cart
+	req.session.cart = cart; // Update current cart with the updated cart
+	res.sendStatus(200);
 });
+
+// retrieves current cart details
+router.get('/get_cart', function(req, res){
+	var cart = new Cart(req.session.cart? req.session.cart : {}) // retrieves current cart if it exists, else initiate a new cart
+	res.send(cart);
+});
+
+// adds a new product entry to the database
+router.post('/add_product', function(req, res){
+	var newProduct = req.body;
+
+	res.locals.connection.query(`INSERT INTO product (title, price, inventory_count) VALUES ("${newProduct.title}", ${newProduct.price}, ${newProduct.inventory_count});`, function(err, results, fields){
+		if(err){
+			console.error(err);
+			res.status(500);
+		} else {
+			console.log("Successfully inserted new product into table");
+			res.status(200);
+		}
+		res.redirect('/'); // redirects back the homepage
+	})
+});
+
 
 module.exports = router;
